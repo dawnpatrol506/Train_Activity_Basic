4546c53242c2ddaf9e8c968a0ceba1fd7296c37c
Kevin Davis <dawnpatrol506@gmail.com>
2018-09-13 08:42:06 -0700

    train arrival calculations done, needs autorefresh

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -30,16 +30,90 @@ $(document).ready(function () {
     });
 
     trainRef.on('value', snap => {
-        $('#current-trains').empty();
+        let rows = $('.removable');
 
-        snap.forEach(childSnap =>{
-            var newTrain = $('<tr>');
-            var newName = $('<td>' + childSnap.val().name + '</td>');
-            var newDest = $('<td>' + childSnap.val().destination + '</td>');
-            var newFreq = $('<td>' + childSnap.val().frequency + '</td>');
+        rows.toArray().forEach(row => row.remove());
 
-            newTrain.append(newName, newDest, newFreq);
-            $('#current-trains').append(newTrain);
-        })
+        snap.forEach(childSnap => {
+            populateTrainData(childSnap);
+        });
     });
+
+    function populateTrainData(childSnap) {
+        var newTrain = $('<tr class="removeable">');
+        var newName = $('<td>' + childSnap.val().name + '</td>');
+        var newDest = $('<td>' + childSnap.val().destination + '</td>');
+        var newFreq = $('<td>' + childSnap.val().frequency + '</td>');
+
+        newTime = parseTime(childSnap.val().time);
+        newTime = calcNextTrain(newTime, childSnap.val().frequency);
+
+        let newDate = new Date(newTime.time);
+        let h = formatHours(newDate.getHours());
+        let m = formatMinutes(newDate.getMinutes());
+
+        let nextArrivalString = (h + ':' + m);
+
+        var nextArrival = $('<td id="next-arrival">' + nextArrivalString + '</td>');
+        var minsAway = $('<td id="mins-away">' + newTime.minsAway + '</td>');
+
+        newTrain.append(newName, newDest, newFreq, nextArrival, minsAway);
+        $('#current-trains').append(newTrain);
+    };
+
+    function parseTime(time) {
+        let d = new Date();
+        let yr = d.getFullYear();
+        let mo;
+        let dy;
+
+        if ((d.getMonth() + 1) < 10)
+            mo = '0' + (d.getMonth() + 1);
+        else
+            mo = d.getMonth() + 1;
+
+        if (d.getDate() < 10)
+            dy = '0' + d.getDate();
+        else
+            dy = d.getDate();
+        let str = yr + '-' + mo + '-' + dy + 'T' + time + ':00';
+
+        let t = new Date(str);
+
+        return t;
+    }
+
+    function formatHours(hours) {
+        if (hours < 10)
+            return '0' + hours;
+        else
+            return hours;
+    };
+    function formatMinutes(minutes) {
+        if (minutes < 10)
+            return '0' + minutes;
+        else
+            return minutes;
+    };
+
+    function calcNextTrain(date, frequency) {
+        let d = new Date();
+        let newDate = new Date(date);
+        if (newDate > d) {
+            let mins = Math.ceil((date - d) / 1000 / 60);
+            return { time: date, minsAway: mins };
+        }
+        else {
+            while (newDate < d) {
+                newDate = new Date(newDate.getTime() + (frequency * 1000 * 60));
+            }
+
+            let mins = Math.ceil((newDate - d) / 1000 / 60);
+            return { time: newDate, minsAway: mins };
+        }
+    }
+
+
+
+
 });
